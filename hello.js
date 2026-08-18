c61e17f2cb7d503bb7c6b58062a1233ad84ddba2
Aryan Mavi <aryan009m@gmail.com>
2026-08-18 11:46:26 +0530

    exp 1b

--- hello.js
+++ hello.js
@@ -1,3 +1,13 @@
-const math = require("./maths.js");
-console.log(math.add.sum(54,33));
-console.log(math.subt.sub(3,5));
+// const math = require("./maths.js");
+// console.log(math.add.sum(54,33));
+// console.log(math.subt.sub(3,5));
+
+const EventEmitter = require("events");
+const myEmitter = new EventEmitter();
+//Event Listner
+
+myEmitter.on("click", ()=>{
+    console.log("Button Clicked");
+});
+// Trigger the event
+myEmitter.emit("click");
