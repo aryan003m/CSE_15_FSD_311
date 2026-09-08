1a15e18d65011d2c832860dbffd320fb2b6b8b2b
Aryan Mavi <aryan009m@gmail.com>
2026-09-08 12:03:14 +0530

    routig

--- exp4.js
+++ exp4.js
@@ -1,7 +1,21 @@
-const http = require("http");
-const server = http.createServer((req,res) =>{
-    console.log(req.method);
-    res.end("Request received");
+// const http = require("http");
+// const server = http.createServer((req,res) =>{
+//     console.log(req.method);
+//     res.end("Request received");
 
+// });
+// server.listen(3000);
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello World');
+    }
 });
-server.listen(300);
+
+server.listen(8000, () => {
+    console.log('Server running at http://localhost:8000');
+});
+
